0f4c11397b64e3f40f007517490eaa1db2ba3039
Bob Koontz <robertskoontz@gmail.com>
2020-02-22 09:58:16 -0800

    requests and a small bit of json

---
+++ nasa_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+from pprint import pprint
+import requests
+import shutil
+import os.path
+
+def api_ca():
+     url = "https://api.nasa.gov/planetary/apod?api_key={omit-api-key}}"
+     r = requests.get(url)
+     response = r.json()
+     high_def_APOD = response['hdurl']
+     pprint(high_def_APOD)
+     return high_def_APOD
+    
+def photo_of_the_day(data):
+    response = requests.get(data, stream=True)
+    with open(os.path.join('/Users/bobkoontz/Desktop','nasa.jpg'), 'wb') as f:
+        response.raw.decode_content = True
+        shutil.copyfileobj(response.raw, f)
+
+def main():
+    data = api_ca()
+    photo_of_the_day(data)
+
+if __name__ == "__main__":
+    main()
+
