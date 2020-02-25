bc0ac31b479ea476c835cb93523989b7e99a0f55
Bob Koontz <robertskoontz@gmail.com>
2020-02-25 12:55:46 -0800

    api key into seperate file

--- nasa_api.py
+++ nasa_api.py
@@ -4,17 +4,20 @@ from pprint import pprint
 import requests
 import shutil
 import os.path
+import json
+from cfg_nasa import *
 
 def api_ca():
-     url = "https://api.nasa.gov/planetary/apod?api_key={omit-api-key}}"
+     url = "https://api.nasa.gov/planetary/apod?api_key={0}".format(api_key)
+     print(url)
      r = requests.get(url)
      response = r.json()
-     high_def_APOD = response['hdurl']
+     high_def_APOD = response#['hdurl']
      pprint(high_def_APOD)
      return high_def_APOD
     
 def photo_of_the_day(data):
-    response = requests.get(data, stream=True)
+    response = requests.get(data) #stream=True)
     with open(os.path.join('/Users/bobkoontz/Desktop','nasa.jpg'), 'wb') as f:
         response.raw.decode_content = True
         shutil.copyfileobj(response.raw, f)
