13fba609340408a06e6cdc20bdce157d446d45dd
Bob Koontz <robertskoontz@gmail.com>
2020-02-26 10:51:54 -0800

    adding return dict

--- nasa_api.py
+++ nasa_api.py
@@ -7,25 +7,25 @@ import os.path
 import json
 from cfg_nasa import *
 
-def api_ca():
+def api_call_jsons():
      url = "https://api.nasa.gov/planetary/apod?api_key={0}".format(api_key)
-     print(url)
      r = requests.get(url)
      response = r.json()
-     high_def_APOD = response#['hdurl']
-     pprint(high_def_APOD)
-     return high_def_APOD
+     high_def_APOD = response.get('hdurl')
+     title_APOD = response.get('title')
+     return {'pic': high_def_APOD, 'titles': title_APOD}
     
-def photo_of_the_day(data):
-    response = requests.get(data) #stream=True)
-    with open(os.path.join('/Users/bobkoontz/Desktop','nasa.jpg'), 'wb') as f:
+def get_photo_of_the_day(data):
+    titles_value = data.get('titles')
+    pic_value = data.get('pic')
+    response = requests.get((pic_value), stream=True)
+    with open(os.path.join('/Users/bobkoontz/Pictures/nasa/','{0}.jpg').format(titles_value), 'wb') as f:
         response.raw.decode_content = True
         shutil.copyfileobj(response.raw, f)
 
 def main():
-    data = api_ca()
-    photo_of_the_day(data)
+    data = api_call_jsons()
+    get_photo_of_the_day(data)
 
 if __name__ == "__main__":
     main()
-
