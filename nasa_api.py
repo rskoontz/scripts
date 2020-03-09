bddbddd7d72167ed21c7205e0eda15e47a49efc0
Bob Koontz <robertskoontz@gmail.com>
2020-03-08 22:55:41 -0700

    pep8 format

--- nasa_api.py
+++ nasa_api.py
@@ -7,34 +7,38 @@ import os.path
 import json
 from cfg_nasa import *
 
-##cfg_nasa holds variable api_key, this was requested from nasa.api.gov, a bit of work needs to be 
-##done on cleaning up the titles for the pic, and cron job from within app
-# set the path to where your os x box is pulling wallpapers, 
+# cfg_nasa holds variable api_key, this was requested from nasa.api.gov, a bit of work needs to be
+# done on cleaning up the titles for the pic, and cron job from within app
+# set the path to where your os x box is pulling wallpapers,
+
 
 def api_call_jsons():
-     url = "https://api.nasa.gov/planetary/apod?api_key={0}".format(api_key)
-     r = requests.get(url)
-     response = r.json()
-     pprint(response)
-     try:
+    url = "https://api.nasa.gov/planetary/apod?api_key={0}".format(api_key)
+    r = requests.get(url)
+    response = r.json()
+    pprint(response)
+    try:
         high_def_APOD = response.get('hdurl')
         title_APOD = response.get('title')
-     except Exception:
+    except Exception:
         print("The json returned by nasa api does not include the proper key value pair. Check back tomorrow.")
-     else: 
+    else:
         return {'pic': high_def_APOD, 'titles': title_APOD}
-    
+
+
 def get_photo_of_the_day(data):
     titles_value = data.get('titles')
     pic_value = data.get('pic')
     response = requests.get((pic_value), stream=True)
-    with open(os.path.join('/Users/bobkoontz/Pictures/nasa/','{0}.jpg').format(titles_value), 'wb') as f:
+    with open(os.path.join('/Users/bobkoontz/Pictures/nasa/', '{0}.jpg').format(titles_value), 'wb') as f:
         response.raw.decode_content = True
         shutil.copyfileobj(response.raw, f)
+
 
 def main():
     data = api_call_jsons()
     get_photo_of_the_day(data)
+
 
 if __name__ == "__main__":
     main()
