bb22337285647743600bdfcbf3ea4dfeccec95ed
Bob Koontz <robertskoontz@gmail.com>
2020-03-08 22:56:16 -0700

    quick os x box info

---
+++ os_info.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import sys
+import platform
+
+
+def os_information():
+    print(platform.system())
+    print(platform._sys_version())
+    print(platform.node())
+    print(platform.machine())
+    print(platform.processor())
+    print(platform.mac_ver())
+
+def main():
+    os_information()
+
+
+if __name__ == "__main__":
+    main()
